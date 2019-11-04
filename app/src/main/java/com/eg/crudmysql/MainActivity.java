9eacc229835907ba92eeeab9059003719d48390e
Willian-adonay <willianadonay.1507@gmail.com>
2019-11-03 18:49:31 -0600

    la interfas de la app

--- app/src/main/java/com/eg/crudmysql/MainActivity.java
+++ app/src/main/java/com/eg/crudmysql/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    String franklin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
